bb5a338ca41dba282be00cdf8cd272c098b340d6
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2021-11-23 15:42:57 -0800

    Netflix data preprocessor. (#4)

---
+++ NetflixDataPreprocessor/CsvRecord.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="CsvRecord.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Teqniqly.AzBatch.NetflixDataPreprocessor
+{
+    public class CsvRecord
+    {
+        public string ShowId { get; set; }
+
+        public string Type { get; set; }
+
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public string Cast { get; set; }
+
+        public string Country { get; set; }
+
+        public string DateAdded { get; set; }
+
+        public short ReleaseYear { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Duration { get; set; }
+
+        public string ListedIn { get; set; }
+
+        public string Description { get; set; }
+    }
+}
