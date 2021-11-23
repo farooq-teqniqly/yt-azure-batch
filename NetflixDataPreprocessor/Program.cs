7e866dbfc1999ea9242af766ebe6dfa45900e8e7
Farooq <farooq@teqniqly.com>
2021-11-23 15:42:40 -0800

    Netflix data preprocessor.

---
+++ NetflixDataPreprocessor/Program.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="Program.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Teqniqly.AzBatch.NetflixDataPreprocessor
+{
+    using System;
+    using System.Threading.Tasks;
+    using CommandLine;
+
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            var app = new App();
+
+            return await Parser
+                .Default
+                .ParseArguments<CommandLineOptions>(args)
+                .MapResult(
+                    async options =>
+                    {
+                        try
+                        {
+                            await app.RunAsync(options);
+                            return 0;
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(exception);
+                            return -2;
+                        }
+                    },
+                    errs => Task.FromResult(-1));
+        }
+    }
+}
