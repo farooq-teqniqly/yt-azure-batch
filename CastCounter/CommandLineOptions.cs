9ecb31ce4f92f9567f760e9e862024efe127ecb6
Farooq <farooq@teqniqly.com>
2021-11-23 15:52:36 -0800

    Cast counter app

---
+++ CastCounter/CommandLineOptions.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="CommandLineOptions.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Teqniqly.AzBatch.CastCounter
+{
+    using CommandLine;
+
+    public class CommandLineOptions
+    {
+        [Option(longName: "input-file", shortName: 'f', Required = true, HelpText = "The input file.")]
+        public string InputFile { get; set; }
+
+        [Option(longName: "output-file", shortName: 'o', Required = true, HelpText = "The output file.")]
+        public string OutputFile { get; set; }
+    }
+}
