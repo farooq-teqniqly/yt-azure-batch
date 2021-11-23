30fd2195fe6b59919afa6d8027ee3b77bce8d23a
farooq-teqniqly <49829654+farooq-teqniqly@users.noreply.github.com>
2021-11-23 15:52:56 -0800

    Cast counter app (#5)

---
+++ CastCounter/App.cs
@@ -0,0 +1,30 @@
+﻿// <copyright file="App.cs" company="Teqniqly">
+// Copyright (c) Teqniqly. All rights reserved.
+// </copyright>
+
+namespace Teqniqly.AzBatch.CastCounter
+{
+    using System;
+    using System.IO;
+    using System.Threading.Tasks;
+
+    public class App
+    {
+        public async Task RunAsync(CommandLineOptions options)
+        {
+            Console.WriteLine($"Reading file '{options.InputFile}'...");
+
+            var inputText = await File.ReadAllTextAsync(options.InputFile);
+
+            var castCount = inputText
+                .Split(",", StringSplitOptions.RemoveEmptyEntries)
+                .Length;
+
+            await File.WriteAllTextAsync(
+                options.OutputFile,
+                castCount.ToString());
+
+            Console.WriteLine($"Output written to '{options.OutputFile}'.");
+        }
+    }
+}
